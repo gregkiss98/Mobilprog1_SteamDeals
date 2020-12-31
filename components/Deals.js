8f1a766174a27632ef22e23988887f96c688abe2
Gergő Kiss <gregkiss98@users.noreply.github.com>
2020-12-31 12:11:41 +0100

    sum clear up

--- components/Deals.js
+++ components/Deals.js
@@ -19,29 +19,23 @@ function Deals() {
       })
     return () => mounted = false;
   }, [])
-//const htmlContent = <div class="deals"><ul>{list.map(title => <li key={title.storeid}><img src={title.thumb}></img> Game name: {title.title} | Sale Price: {title.salePrice} | Normal Price: {title.normalPrice}</li>)}</ul></div>;
-   /*<div class="deals">
-    <ul>
-      {list.map(title => <li style={{ backgroundColor: theme.backgroundColor,
-    color: theme.color }}  key={title.storeid}><img src={title.thumb}></img> Game name: {title.title} | Sale Price: {title.salePrice} | Normal Price: {title.normalPrice}</li>)}
-    </ul>
 
-  </div>
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        {list.map(title => <li key={title.item}>{title.item}</li>)}
-    </View>*/
-    const htmlContent = `
+  const htmlContent = `
     <div class="deals">
     <ul>
       {list.map(title => <li key={title.storeid}><img src={title.thumb}></img> Game name: {title.title} | Sale Price: {title.salePrice} | Normal Price: {title.normalPrice}</li>)}
     </ul>
     </div>
     `;    
+
     const contentWidth = useWindowDimensions().width;
+
   return (
+    
     <ScrollView style={{ flex: 1 }}>
       <HTML source={{ html: htmlContent }} contentWidth={contentWidth} />
     </ScrollView>
+
   );
 }
  
